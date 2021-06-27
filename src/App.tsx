baac8b856d2e91f89bed19210f69e7f727fad692
TakahiroHimi <takahiro.mth.1209@gmail.com>
2021-06-27 16:25:56 +0900

    ユーザー情報追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 import "./App.css";
+import * as Sentry from "@sentry/react";
 
 function App() {
+  Sentry.setUser({
+    id: "1234",
+    username: "hoge",
+    email: "hoge@hoge.com",
+  });
+
   return (
     <>
       <button
